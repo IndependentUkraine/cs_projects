e54c2832b2abce2409ae5aa160a41a121449643f
denus <stud.and.work.di@gmail.com>
2023-04-25 22:03:31 +0300

    Add Task 2

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -10,7 +10,12 @@
             Console.WriteLine($"Square area: {a * a}cm^2");
             Console.WriteLine($"Square perimeter: {a * 4}cm");
 
-            
+            // Task 2
+            Console.Write("What is your name? : ");
+            string name = Console.ReadLine();
+            Console.Write($"How old are you, {name}? : ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{name}, you`are {age} years old");
         }
     }
 }
