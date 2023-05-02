e0c47e8c38318fea18fc2f89b76124ccd7b53802
denus <stud.and.work.di@gmail.com>
2023-05-02 14:21:01 +0300

    Add Task2

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,10 +1,21 @@
 ﻿
 
+using System.Xml;
+
 namespace Homework_2
 {
     internal class Program
     {
         public static void IsInRange(float number) => Console.WriteLine(number >= -5 && number <= 5 ? "is in range" : "isn`t in range");
+
+        public static void FindMaxAndMin(int number1, int number2, int number3)
+        {
+            int maxNumber = Math.Max(number1, Math.Max(number2,number3));
+            int minNumber = Math.Min(number1, Math.Min(number2,number3));
+
+            Console.WriteLine($"{maxNumber} is maximum \n" +
+                              $"{minNumber} is minimum");
+        }
         static void Main(string[] args)
         {
             //Task 1
@@ -15,7 +26,11 @@ namespace Homework_2
             float number3 = Convert.ToSingle(Console.ReadLine());
             IsInRange(number3);
 
-
+            //Task 2
+            int number4 = Convert.ToInt32(Console.ReadLine());
+            int number5 = Convert.ToInt32(Console.ReadLine());
+            int number6 = Convert.ToInt32(Console.ReadLine());
+            FindMaxAndMin(number4, number5, number6);
         }
     }
 }
