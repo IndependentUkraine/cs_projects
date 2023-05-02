abe7257cae34546f42fc1568bc6b1ef9f88ea90d
denus <stud.and.work.di@gmail.com>
2023-05-02 17:59:54 +0300

    Add Task 4 and upgrade Task 1,2,3

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -27,25 +27,55 @@ namespace Homework_2
             MethodNotAllowed = 405
         }
 
+        struct Dog
+        {
+            public string name;
+            public int age;
+            public string mark;
+
+            public override string ToString()
+            {
+                return $"Dog`s name is {name}, he/she is {mark} and has {age} years old";
+            }
+        }
+
         static void Main(string[] args)
         {
             //Task 1
+            Console.Write("Enter first number: ");
             float number1 = Convert.ToSingle(Console.ReadLine());
             IsInRange(number1);
+            Console.Write("Enter second number: ");
             float number2 = Convert.ToSingle(Console.ReadLine());
             IsInRange(number2);
+            Console.Write("Enter third number: ");
             float number3 = Convert.ToSingle(Console.ReadLine());
             IsInRange(number3);
 
             //Task 2
+            Console.Write("Enter first number: ");
             int number4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
             int number5 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number: ");
             int number6 = Convert.ToInt32(Console.ReadLine());
             FindMaxAndMin(number4, number5, number6);
 
             //Task 3
+            Console.WriteLine("Enter number of HTTP error: ");
             HTTPError error = Enum.Parse<HTTPError>(Console.ReadLine());
             Console.WriteLine(error.ToString());
+
+            //Task 4
+            Dog dog;
+            Console.Write("Enter dog`s name: ");
+            dog.name = Console.ReadLine();
+            Console.Write("Enter dog`s mark: ");
+            dog.mark = Console.ReadLine();
+            Console.Write("Enter dog`s age: ");
+            dog.age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(dog.ToString());
 
         }
     }
