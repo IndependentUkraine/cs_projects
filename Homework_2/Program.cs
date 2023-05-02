39b53d7e943adccdfa9cc70ebc0e1ed01374b38a
denus <stud.and.work.di@gmail.com>
2023-05-02 14:04:16 +0300

    Add Task 1

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Homework_2
+﻿using System.Threading.Channels;
+
+namespace Homework_2
 {
     internal class Program
     {
+        public static void IsInRange(float number) => Console.WriteLine(number >= -5 && number <= 5 ? "is in range" : "isn`t in range");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Task 1
+            float number1 = Convert.ToSingle(Console.ReadLine());
+            IsInRange(number1);
+            float number2 = Convert.ToSingle(Console.ReadLine());
+            IsInRange(number2);
+            float number3 = Convert.ToSingle(Console.ReadLine());
+            IsInRange(number3);
+
+
         }
     }
 }
